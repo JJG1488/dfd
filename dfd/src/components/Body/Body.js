8b44c8826b23d22d6c95e17c1b58cc2c6c65cf9d
James J Gault <74686199+JJG1488@users.noreply.github.com>
2022-01-05 23:44:06 -0500

    changed folder structure to maintainable format

---
+++ dfd/src/components/Body/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+export const Body = () => {
+    return (
+        <div className='container'>
+            <h1><span>DFD LLC</span></h1>
+            <p>Creators of the <a href='https://jjg1488.github.io/dfd/'><span className='underlined-links'>Unapologetic Hoodlums</span></a></p>
+            <p><a href='https://jjg1488.github.io/dfd/'><span className='underlined-links'>jgservicesllc14@gmail.com</span></a></p>
+        </div>
+    )
+}
--- dfd/src/components/Footer/Footer.js
+++ dfd/src/components/Footer/Footer.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
 
 export const Footer = () => {
     return (
         <div>
             <footer className="code2"><span className="copyright" id="footer-span">© {new Date().getFullYear()}  DFD LLC. All Rights Reserved.</span></footer>
-
         </div>
     )
 }
