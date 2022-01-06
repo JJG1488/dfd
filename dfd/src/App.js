0733764571ac76545f9c2a11e6273896adb85c46
James J Gault <74686199+JJG1488@users.noreply.github.com>
2022-01-05 23:00:10 -0500

    Landing page for dfd

--- dfd/src/App.js
+++ dfd/src/App.js
@@ -5,7 +5,9 @@ import { Footer } from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
+      <h2 className="find-us">Find Us</h2>
       <div className="icon-container">
+
         <div className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
           <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
         </svg></div>
@@ -18,13 +20,13 @@ function App() {
       </div>
       <i className="bi bi-facebook"></i>
       <header className="App-header">
-        <h1>DFD LLC</h1>
-        <h3>Creators of the Unapologetic Hoodlums</h3>
-        <h3>jgservicesllc14@gmail.com</h3>
+        <h1><span>DFD LLC</span></h1>
+        <p>Creators of the Unapologetic Hoodlums</p>
+        <p>jgservicesllc14@gmail.com</p>
 
       </header>
 
-      <h2 className="find-us">Find Us</h2>
+      {/* <h2 className="find-us">Find Us</h2> */}
 
 
 
--- dfd/src/components/Footer/Footer.js
+++ dfd/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div>
-            <footer className="code2"><span className="copyright" id="footer-span">© {new Date().getFullYear()}  DFD. All Rights Reserved.</span></footer>
+            <footer className="code2"><span className="copyright" id="footer-span">© {new Date().getFullYear()}  DFD LLC. All Rights Reserved.</span></footer>
 
         </div>
     )
