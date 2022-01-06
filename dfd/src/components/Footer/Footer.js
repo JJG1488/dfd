093b9df047007b08ef34f9a4eb0ad52204830cb8
James J Gault <74686199+JJG1488@users.noreply.github.com>
2022-01-05 22:44:44 -0500

    Base web page

---
+++ dfd/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div>
+            <footer className="code2"><span className="copyright" id="footer-span">© {new Date().getFullYear()}  DFD. All Rights Reserved.</span></footer>
+
+        </div>
+    )
+}
