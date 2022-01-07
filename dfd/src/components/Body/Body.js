38024156e3ed59b6e7e1bb7f4b9e64dd6b79224d
James J Gault <74686199+JJG1488@users.noreply.github.com>
2022-01-07 12:43:03 -0500

    Changed email

--- dfd/src/components/Body/Body.js
+++ dfd/src/components/Body/Body.js
@@ -6,7 +6,7 @@ export const Body = () => {
         <div className='container'>
             <h1><span>DFD LLC</span></h1>
             <p>Creators of the <a href='https://jjg1488.github.io/dfd/'><span className='underlined-links'>Unapologetic Hoodlums</span></a></p>
-            <p><a href='https://jjg1488.github.io/dfd/'><span className='underlined-links'>jgservicesllc14@gmail.com</span></a></p>
+            <p><a href='https://jjg1488.github.io/dfd/'><span className='underlined-links'>dudesfromdetroit@gmail.com</span></a></p>
         </div>
     )
 }
